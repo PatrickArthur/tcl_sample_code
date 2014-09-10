2918869d8767b032a61b46a8e80daa31d51fccd2
Patrick <pa@arthurfamily.ws>
2014-09-10 17:31:37 -0400

    sumintcl

--- tclsample.tcl
+++ tclsample.tcl
@@ -1,8 +1,13 @@
 #!/usr/bin/tclsh
 
 # first.tcl
-set nums { 1 2 3 4 5 6 7 8 9 10 11 12 13 14
-  15 16 17 18 19 20 21 22 23 24
+set i 0
+set sum 0
+
+while { $i < 2 } {
+
+    incr i
+    incr sum $i
 }
 
-puts $nums
+puts $sum
